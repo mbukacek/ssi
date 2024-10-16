f7e899f1f8e26e25854387f0bdd04fafbcbd52e1
mbukacek <35059267+mbukacek@users.noreply.github.com>
2024-10-16 13:28:31 +0200

    ped model adjustments

--- 2024_25/ssi_script_03_fb2d.py
+++ 2024_25/ssi_script_03_fb2d.py
@@ -7,7 +7,7 @@ pd.options.mode.chained_assignment = None  # default='warn'
 
 # https://pedpy.readthedocs.io/en/stable/
 
-def init_ped_data(t,x,y,vx,vy):
+def init_ped_data(t,x,y,vx,vy,ex):
     # initialize ped data container
     # INPUT: vector of init time, init position and init velocity
     
@@ -17,7 +17,8 @@ def init_ped_data(t,x,y,vx,vy):
                              'x': [[x[0]]],                         # to be list of recorded x position
                              'y': [[y[0]]],                         # to be list of recorded y position
                              'vx': [[vx[0]]],                       # to be list of recorded x velocity
-                             'vy': [[vy[0]]]                        # to be list of recorded y velocity
+                             'vy': [[vy[0]]],                       # to be list of recorded y velocity
+                             'time_left': ex[0]                     # time when ped left - or NaN    
                              }, index = [0])
 
     # add peds one by one
@@ -28,7 +29,8 @@ def init_ped_data(t,x,y,vx,vy):
                                 'x': [[x[i+1]]],   
                                 'y': [[y[i+1]]],      
                                 'vx': [[vx[i+1]]],
-                                'vy': [[vy[i+1]]]  
+                                'vy': [[vy[i+1]]],
+                                'time_left': ex[i+1] 
                                 }, index = [i+1])
         ped_data = pd.concat([ped_data,ped_data_n])
     
@@ -40,14 +42,30 @@ def one_ped_step(ped_data, ped_idx, act_t, act_vx, act_vy, const):
     
     ped_data.t[ped_idx] = ped_data.t[ped_idx] + [act_t]             # '+' works as append   
     
-    ped_data.vx[ped_idx] = ped_data.vx[ped_idx] + [act_vx]  
-    ped_data.vy[ped_idx] = ped_data.vy[ped_idx] + [act_vy]       
-    
     new_x = ped_data.x[ped_idx][-1] + act_vx*const['dt']             # [-1] reffers to the last value            
-    ped_data.x[ped_idx] = ped_data.x[ped_idx] + [new_x]
-    
     new_y = ped_data.y[ped_idx][-1] + act_vy*const['dt']                         
+    
+    # hard core zero-range wall repulsion
+    if new_x < const['wx1']:
+        new_x = const['wx1']
+        act_vx = 0
+    if new_x > const['wx2']:
+        new_x = const['wx2']
+        act_vx = 0    
+    if new_y < const['wy1']:
+        new_y = const['wy1']
+        act_vy = 0
+    if new_y > const['wy2']:
+        new_y = const['wy2']
+        act_vy = 0        
+        
+    
+    ped_data.vx[ped_idx] = ped_data.vx[ped_idx] + [act_vx]  
+    ped_data.vy[ped_idx] = ped_data.vy[ped_idx] + [act_vy] 
+    
+    ped_data.x[ped_idx] = ped_data.x[ped_idx] + [new_x]
     ped_data.y[ped_idx] = ped_data.y[ped_idx] + [new_y]
+    
     
     return ped_data
 
@@ -85,9 +103,28 @@ def f_ped_rep(ped_data, ped_idx, other_idx, const):
         sy =  (ped_data.y[other_idx][-1] - ped_data.y[ped_idx][-1]) / math.sqrt(
                 (ped_data.x[ped_idx][-1] - ped_data.x[other_idx][-1])**2 
                 + (ped_data.y[ped_idx][-1] - ped_data.y[other_idx][-1])**2)
-
-        fx = U*sx   
+        
+        fx = U*sx 
         fy = U*sy 
+
+    return fx, fy
+
+
+def f_wall(ped_data, ped_idx, const):
+    
+    if ped_data.x[ped_idx][-1] > const['wx2']/2:
+        Ux = const['U0_w']/const['distance_scale_w'] * math.exp(-1*(const['wx2'] - ped_data.x[ped_idx][-1])/const['distance_scale_w'])
+        fx = -1*Ux
+    else:
+        Ux = const['U0_w']/const['distance_scale_w'] * math.exp(-1*ped_data.x[ped_idx][-1]/const['distance_scale_w'])
+        fx = Ux
+    
+    if ped_data.y[ped_idx][-1] > const['wy2']/2:
+        Uy = const['U0_w']/const['distance_scale_w'] * math.exp(-1*(const['wy2'] - ped_data.y[ped_idx][-1])/const['distance_scale_w'])
+        fy = -1*Uy
+    else:
+        Uy = const['U0_w']/const['distance_scale_w'] * math.exp(-1*ped_data.y[ped_idx][-1]/const['distance_scale_w'])
+        fy = Uy
 
     return fx, fy
 
@@ -108,23 +145,81 @@ def update_v(ped_data, ped_idx, model_name, const):
         
         fxa, fya = f_motivation(ped_data, ped_idx, const)
         
-        fx0, fy0 = f_ped_rep(ped_data, ped_idx, 0, const)
-        fx1, fy1 = f_ped_rep(ped_data, ped_idx, 1, const)
-        fx2, fy2 = f_ped_rep(ped_data, ped_idx, 2, const)
-        fx3, fy3 = f_ped_rep(ped_data, ped_idx, 3, const)
-        fx4, fy4 = f_ped_rep(ped_data, ped_idx, 4, const)
-       
-        fx = fxa - fx0 - fx1 - fx2 - fx3 - fx4
-        fy = fya - fy0 - fy1 - fy2 - fy3 - fy4
+        fxp, fyp = calc_ped_interaction(ped_data, ped_idx, const)
+  
+        fx = fxa + fxp
+        fy = fya + fyp
   
         new_vx = ped_data.vx[ped_idx][-1] + fx*const['dt']
         new_vy = ped_data.vy[ped_idx][-1] + fy*const['dt']
         
+    elif model_name == 'motivation_interaction_wall':    
+         
+         fxa, fya = f_motivation(ped_data, ped_idx, const)
+         
+         fxp, fyp = calc_ped_interaction(ped_data, ped_idx, const)
+   
+         fxw, fyw = f_wall(ped_data, ped_idx, const) 
+   
+         fx = fxa + fxp + fxw
+         fy = fya + fyp + fyw
+   
+         new_vx = ped_data.vx[ped_idx][-1] + fx*const['dt']
+         new_vy = ped_data.vy[ped_idx][-1] + fy*const['dt']       
+        
     else:
         new_vx = np.nan
         new_vy = np.nan
-    
+           
     return new_vx, new_vy
+    
+
+def calc_ped_interaction(ped_data, ped_idx, const):
+    
+    if np.isnan(ped_data.time_left[0]):
+        fx0, fy0 = f_ped_rep(ped_data, ped_idx, 0, const)
+    else:
+        fx0 = 0
+        fy0 = 0 
+    if np.isnan(ped_data.time_left[1]):    
+        fx1, fy1 = f_ped_rep(ped_data, ped_idx, 1, const)
+    else:
+        fx1 = 0
+        fy1 = 0         
+    if np.isnan(ped_data.time_left[2]):
+        fx2, fy2 = f_ped_rep(ped_data, ped_idx, 2, const)
+    else:
+        fx2 = 0
+        fy2 = 0         
+    if np.isnan(ped_data.time_left[3]):
+        fx3, fy3 = f_ped_rep(ped_data, ped_idx, 3, const)
+    else:
+        fx3 = 0
+        fy3 = 0         
+    if np.isnan(ped_data.time_left[4]):
+        fx4, fy4 = f_ped_rep(ped_data, ped_idx, 4, const)
+    else:
+        fx4 = 0
+        fy4 = 0         
+        
+    fx = -fx0 - fx1 - fx2 - fx3 - fx4
+    fy = -fy0 - fy1 - fy2 - fy3 - fy4
+    
+    return fx, fy
+
+
+def ped_exit(ped_data, j, act_t, const):
+    # A pedestrian leaves the simulation if the distance 
+    # to the exit is smaller than a constant 'exit_range'
+    
+    exit_dist = math.sqrt((ped_data.x[j][-1] - const['attractor_x'])**2 
+                          + (ped_data.y[j][-1] - const['attractor_y'])**2)
+    
+    if exit_dist <= const['exit_range']:
+        print('Ped ' + str(j) + ' left')
+        ped_data.time_left[j] = act_t 
+    
+    return
 
 
 #============================================#
@@ -137,25 +232,33 @@ def update_v(ped_data, ped_idx, model_name, const):
 
 # Constants - dictionary
 const = {'N_ped': 5,                # numer of peds in the system
-         'N_step': 100,              # number of steps
+         'N_step': 100,             # number of steps
          'dt': 0.1,                 # diffrential step length [s]
          'v_opt': 3,                # optimal velocity (scalar) [m/s]
-         'tau': 4,
-         'U0': 1,
-         'distance_scale':1,         
+         'tau': 4,                  # motivation force parameter [s]
+         'U0': 1,                   # ped intearction potential parameter [J.m]
+         'U0_w': 0.1,               # ped intearction potential parameter [J.m]
+         'distance_scale': 1,       # ped interaction distance parameter [m]
+         'distance_scale_w': 0.5,   # wall interaction distance parameter [m]
          'attractor_x': 10,         # x position of attractor [m]
-         'attractor_y': 5           # y position of attractor [m]
+         'attractor_y': 5,          # y position of attractor [m]
+         'exit_range': 0.2,         # distance where ped falls into the exit [m] 
+         'wx1': 0,                  # wall coordinates [m] - rentangle expected by code, [0,0] expected corner
+         'wx2': 10,
+         'wy1': 0,
+         'wy2': 7   
         }
 
 # Init time, positions and velocities
-t = [0, 0, 0, 0, 0]
-x = [1.1, 0.9, 1, 3, 3.05]
-y = [1, 2.5, 4, 1.1, 2.6]
-vx = [1.5, 1.5, 1.5, 1.5, 1.5]
-vy = [0, 0, 0, 0, 0]
+t =  [  0,   0,   0,   0,    0]
+x =  [1.1, 0.9,   1,   3, 3.05]
+y =  [  1, 2.5,   4, 1.1, 2.60]
+vx = [1.5, 1.5, 1.5, 1.5, 1.50]
+vy = [  0,   0,   0,   0,    0]
+ex = [np.nan, np.nan, np.nan, np.nan, np.nan]
 
 # Init data containers
-ped_data = init_ped_data(t,x,y,vx,vy)
+ped_data = init_ped_data(t, x, y, vx, vy, ex)
 
 
 
@@ -167,23 +270,24 @@ ped_data = init_ped_data(t,x,y,vx,vy)
 rep = range(const['N_step'])
 for i in rep: 
 
-    act_t = (i+1)*const['dt']                                       # i+1 is current itteration as i = 0  was defined in init step 
+    act_t = (i+1)*const['dt']                         # i+1 is current itteration as i = 0  was defined in init step 
     
     # model loop over other peds 
     rep2 = range(const['N_ped'])
     for j in rep2: 
     
-        act_vx, act_vy = update_v(ped_data, j, 'motivation_interaction', const)
-        ped_data = one_ped_step(ped_data, j, act_t, act_vx, act_vy, const)
+        if np.isnan(ped_data.time_left[j]):    
+            act_vx, act_vy = update_v(ped_data, j, 'motivation_interaction_wall', const)
+            ped_data = one_ped_step(ped_data, j, act_t, act_vx, act_vy, const)
     
-
+            ped_exit(ped_data, j, act_t, const)
 
 #======================#
 #     POSTPROCESSING   #
 #======================#
 
 
-# Timespace fundamental diagram
+# Timespace fundamental diagram x
 plt.figure()
 plt.plot(ped_data.t[0], ped_data.x[0], 'r-', label = 'ped 1')
 plt.plot(ped_data.t[1], ped_data.x[1], 'g-', label = 'ped 2')
@@ -214,16 +318,16 @@ plt.ylabel(r'$y \,\,\, \mathrm{[m]}$')
 plt.legend()
 plt.show()
 
-# Timespace fundamental diagram
+# Timespace fundamental diagram y
 plt.figure()
-plt.plot(ped_data.t[0], ped_data.x[0], 'r-', label = 'ped 1')
-plt.plot(ped_data.t[1], ped_data.x[1], 'g-', label = 'ped 2')
-plt.plot(ped_data.t[2], ped_data.x[2], 'b-', label = 'ped 3')
-plt.plot(ped_data.t[3], ped_data.x[3], 'k-', label = 'ped 4')
-plt.plot(ped_data.t[4], ped_data.x[4], 'm-', label = 'ped 5')
+plt.plot(ped_data.t[0], ped_data.y[0], 'r-', label = 'ped 1')
+plt.plot(ped_data.t[1], ped_data.y[1], 'g-', label = 'ped 2')
+plt.plot(ped_data.t[2], ped_data.y[2], 'b-', label = 'ped 3')
+plt.plot(ped_data.t[3], ped_data.y[3], 'k-', label = 'ped 4')
+plt.plot(ped_data.t[4], ped_data.y[4], 'm-', label = 'ped 5')
 plt.title('Timespace fundamental diagram')
 plt.xlabel(r'$t \,\,\mathrm{[s]}$')
-plt.ylabel(r'$x \,\,\, \mathrm{[m]}$')
+plt.ylabel(r'$y \,\,\, \mathrm{[m]}$')
 #plt.xlim(0, 10)
 #plt.ylim(0, 120)
 plt.legend()
